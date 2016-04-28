5dc5da4b430e10693d27fc245d39d20cc3ddc9d4
lucasvberkel <lucasvberkel@gmail.com>
2016-04-28 19:13:11 +0200

    Bijna af

--- classify_fmri_data_PR4_students.m
+++ classify_fmri_data_PR4_students.m
@@ -1,9 +1,9 @@
 %% Brein en cognitie 2016
 %% PR4: Classificatie van fMRI data
 %% Created by Jolien Francken 2016
-%% Namen:
-%% Studentnummers:
-%% Groep: 
+%% Namen: Lucas van Berkel, Joël Meyer 
+%% Studentnummers: 10747958, 10003539
+%% Groep: 39
 
 %% Introductie
 %In dit practicum gaan we fMRI data gebruiken van een experiment waarbij proefpersonen bewegende stippen te zien kregen die op of neer bewogen. In
@@ -11,7 +11,7 @@
 %classificatie uitvoeren om uiteindelijk onze hoofdvraag te beantwoorden:
 %Kunnen we op basis van fMRI-patronen voorspellen of een proefpersoon naar opwaartse of neerwaartse beweging kijkt?
 clear all; close all;
-data_dir = 'E:\Jolien\KI\Jolien\PR4'; %%%%%Geef hier de directory waar de datafile is opgeslagen
+data_dir = 'C:\Users\Lucas\Documents\Uni\Brein-Cognitie'; %%%%%Geef hier de directory waar de datafile is opgeslagen
 load(fullfile(data_dir,'data_struct'));
 subjects = {'S02','S03','S04','S06','S07'}; %We gaan de data van 5 proefpersonen analyseren. Nummer S01 en S05 zijn geexcludeerd; S01 viel in slaap tijdens het experiment; S05 bewoog teveel tijdens het scannen zodat de data onbruikbaar werd.
 rois = {'lOC','rOC'}; %We bekijken fMRI-patronen in twee 'regions of interest'(ROIs): de linker en rechter visuele cortex
@@ -36,11 +36,31 @@ vox_sel = [10 50 100 200 300 400 500]; %Aantal voxels binnen de ROIs die we will
 
 %Vraag 1
 %a. Hoeveel voxels zitten er in het brein van proefpersoon S06?
+%%
+betas = data(4).betas;
+[height, width] = size(betas);
+counter = 0;
+for i=1:height
+    if(isnan(betas(i,1)))
+        counter = counter + 1;
+    end
+end
+totalAmountOfVoxels = height - counter
+%%
 %b. Hoeveel voxels vallen er binnen de linker visuele cortex van S06? En hoeveel binnen de rechter?
 %c. Wat is de locatie/index van de voxel met de hoogste beta waarde binnen de linker visuele cortex van S06? En wat is deze waarde?
 %d. Wat is de laagste beta waarde binnen de linker visuele cortex van S06? Wat betekent dit denk je?
 %e. Plot de beta waarden voor alle voxels van S06. Plaats deze plot ook in het verslag. Wat valt je op?
-
+%%
+figure;
+hold on;
+X1 = data(4).mask(1).val(1:data(4).mask(1).nvox);
+X2 = data(4).mask(2).val(1:data(4).mask(2).nvox);
+plot(X1)
+plot(X2)
+legend({'Left visual cortex' 'Right visual cortex'})
+hold off;
+%%
 %We willen classificeren binnen de ROI en daarom gaan we nu de relevante voxels selecteren. Wat zijn de relevante voxels? Ten eerste natuurlijk de voxels die
 %binnen de mask vallen. Ten tweede de meest actieve voxels binnen de mask, waarvan we het aantal kunnen varieren; de waarden die we nu gebruiken
 %staan in de variabele vox_sel.
@@ -72,38 +92,89 @@ vectors = vectors(data(isubj).trial_info.incl_trials,:);
 %testen (cross-validation). Vervolgens worden de trainings fMRI patronen (vectors) en de trainings labels gebruikt om een lineaire SVM te
 %trainen. Daarna krijgt de SVM de test vectors en bepaalt welk label daar het beste bij past (1 of 2, oftewel opwaartse of
 %neerwaartse beweging). Deze labels worden opgeslagen in classification_labels.
-ntrials = 1:20;
-nblocks = 10;
-ntrials_per_block = 2;
-classification_labels = zeros(length(data(isubj).trial_info.cond),1);
+[~, sizeSubjects] = size(subjects);
+[~, sizeRois] = size(rois);
+[~, sizeVox_sel] = size(vox_sel);
+results = zeros(sizeRois*sizeSubjects, sizeVox_sel);
+counter = 1;
+for k=1:sizeSubjects
+    for j=1:sizeRois
+        for i=1:sizeVox_sel
+            isubj = k;
+            iroi = j;
+            ivox = i;
 
-for iblock = 1:nblocks
+            %Selecteer meest actieve voxels binnen de mask
+            nvox = min(vox_sel(ivox),data(isubj).mask(iroi).nvox);
+            max_indx = data(isubj).mask(iroi).ind(1:nvox); 
+            vectors = data(isubj).betas(max_indx,:)';     
+            %Excludeer voxels met waarde 0 of NaN
+            incl_voxels = find(std(vectors,0,1) ~= 0 & ~isnan(vectors(1,:)));
+            vectors = vectors(:,incl_voxels);             
+            vectors = zscore(vectors);     
+            %Selecteer data voor de 20 relevante trials (met opwaartse of neerwaartse beweging)
+            vectors = vectors(data(isubj).trial_info.incl_trials,:);
 
-    %Train op trials die niet iblock zijn
-    train_indices_number = setdiff(1:length(ntrials),1+(iblock-1)*ntrials_per_block:iblock*ntrials_per_block);
-    train_indices = ntrials(train_indices_number);
-    train_vectors = vectors(train_indices,:);
-    train_labels = data(isubj).trial_info.cond(train_indices);
+            ntrials = 1:20;
+            nblocks = 10;
+            ntrials_per_block = 2;
+            classification_labels = zeros(length(data(isubj).trial_info.cond),1);
 
-    %Test op trials die iblock zijn
-    test_indices_number = 1+(iblock-1)*ntrials_per_block:iblock*ntrials_per_block;
-    test_indices = ntrials(test_indices_number);
-    test_vectors = vectors(test_indices,:);
-    test_labels = data(isubj).trial_info.cond(test_indices);
+            for iblock = 1:nblocks
 
-    %Train en test de SVM
-    svmstruct = svmtrain(train_vectors,train_labels,'AutoScale',false);
-    classification_labels(test_indices) = svmclassify(svmstruct,test_vectors);
+                %Train op trials die niet iblock zijn
+                train_indices_number = setdiff(1:length(ntrials),1+(iblock-1)*ntrials_per_block:iblock*ntrials_per_block);
+                train_indices = ntrials(train_indices_number);
+                train_vectors = vectors(train_indices,:);
+                train_labels = data(isubj).trial_info.cond(train_indices);
 
-    clear svmstruct test* train* 
+                %Test op trials die iblock zijn
+                test_indices_number = 1+(iblock-1)*ntrials_per_block:iblock*ntrials_per_block;
+                test_indices = ntrials(test_indices_number);
+                test_vectors = vectors(test_indices,:);
+                test_labels = data(isubj).trial_info.cond(test_indices);
 
-end
+                %Train en test de SVM
+                svmstruct = svmtrain(train_vectors,train_labels,'AutoScale',false);
+                classification_labels(test_indices) = svmclassify(svmstruct,test_vectors);
+
+                clear test* train* svmstruct 
+
+            end
+
+            %Bereken de accuracy van de SVM classificatie
+            accuracy = sum(classification_labels(ntrials) == data(isubj).trial_info.cond(ntrials)')/length(ntrials);
+            
+            results(counter,i) = accuracy;
+            
+            clear classification_labels ntrials nblocks ntrials_per_block vectors max_indx incl_voxels iblock nvox
+            clear iroi isubj ivox
+        end
+
+        if (mod(counter,2)==0)
+            text = strcat('Right visual Cortex ',subjects(k));
+        else
+            text = strcat('Left visual Cortex ',subjects(k));
+        end
+        tempLegend(counter) = text;
         
-%Bereken de accuracy van de SVM classificatie
-accuracy = sum(classification_labels(ntrials) == data(isubj).trial_info.cond(ntrials)')/length(ntrials);
+        counter = counter + 1;
 
-clear classification_labels ntrials nblocks ntrials_per_block vectors max_indx incl_voxels iblock nvox
-clear iroi isubj ivox
+    end
+end
+
+figure;
+hold on;
+for i=1:sizeRois*sizeSubjects
+    plot(vox_sel, results(i,:))
+end
+title('Accuracy of SVM classifier for left and right visual cortex')
+legend(tempLegend)
+ylabel('Accuracy')
+xlabel('Number of voxels in trainingset')
+axis([0 vox_sel(7) 0 1])
+hold off;
+
 
 %Vraag 3
 %a. Leg uit hoe cross-validatie werkt (zie het artikel van Mur et al. (2009))
